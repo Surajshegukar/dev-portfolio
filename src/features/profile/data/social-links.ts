1696f445a3dcddeaa22f3b9de8789b0c9e8c2660
Suraj Shegukar <surajshegukar2732@gmail.com>
2026-08-28 18:45:10 +0530

    modified:   src/features/profile/data/social-links.ts

--- src/features/profile/data/social-links.ts
+++ src/features/profile/data/social-links.ts
@@ -5,7 +5,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
     icon: "https://api.dicebear.com/7.x/initials/svg?seed=LI&backgroundColor=0a66c2",
     title: "LinkedIn",
     description: "@suraj-shegukar",
-    href: "https://linkedin.com/in/suraj-shegukar",
+    href: "https://www.linkedin.com/in/surajshegukar/",
   },
   {
     icon: "https://api.dicebear.com/7.x/initials/svg?seed=GH&backgroundColor=24292e",
